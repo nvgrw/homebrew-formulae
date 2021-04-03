a7391ccebe78d59303ae0955fb6078ff3345b561
Niklas Vangerow <n@vangerow.net>
2021-04-03 16:32:49 +0100

    Address grit formula lint issues

--- Formula/grit.rb
+++ Formula/grit.rb
@@ -1,10 +1,10 @@
 class Grit < Formula
-  desc "A multitree-based personal task manager"
+  desc "Multitree-based personal task manager"
   homepage "https://github.com/climech/grit/"
   license "MIT"
-  version "0.2.0"
   url "https://github.com/climech/grit/archive/ab30f8612e2c46ad0981a2f29c1e344bb60f6e35.tar.gz"
   sha256 "6e02c2ba58e9596c808ce7aa327b2a212da2ee516acf90ee702a6439466bedfe"
+  version "0.2.0"
   head "https://github.com/climech/grit.git"
 
   depends_on "go" => :build
@@ -13,7 +13,7 @@ class Grit < Formula
     gopath = "#{buildpath}/gopath"
     mkdir_p gopath
     ENV["GOPATH"] = gopath
-    ENV["PREFIX"] = "#{prefix}"
+    ENV["PREFIX"] = prefix
 
     system "make"
     system "make", "install"
